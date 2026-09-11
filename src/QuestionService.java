bb9b11e6195c84ba65c2d848408fc1b31d6841d1
Maks <makzylinski@gmail.com>
2026-09-11 15:20:30 +0200

    Questions

--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class QuestionService {
     Question[] questions = new Question[5];
+    String[] selection = new String[5];
 
     public QuestionService() {
         questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
@@ -8,9 +11,25 @@ public class QuestionService {
         questions[3] = new Question(4, "size of long", "2", "6", "4", "8", "8");
         questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");    }
 
-    public void displayQuestions() {
+    public void playQuiz() {
+        int i = 0;
         for(Question q : questions) {
-            System.out.println(q);
+            System.out.println("Question no.: " + q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+
+            Scanner sc = new Scanner(System.in);
+
+            selection[i] = sc.nextLine();
+
+            i++;
+        }
+
+        for (String s : selection) {
+            System.out.println(s);
         }
     }
 }
